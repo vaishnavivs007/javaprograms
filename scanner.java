fd6210eaa05a45609c21a425ceb5df68752e7d4d
vaishnavivs007 <vaishnavishejwadkar@gmail.com>
2025-04-11 10:47:37 +0530

    save this java file

---
+++ scanner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your height: ");
+        double height = scanner.nextDouble();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+        scanner.close();
+    }
+}
