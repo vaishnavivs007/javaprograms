98ece54aa9a11dc882543780211bb886964953cf
vaishnavivs007 <vaishnavishejwadkar@gmail.com>
2025-04-11 10:34:16 +0530

    save this java file

---
+++ simple.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class simple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (num > 0) {
+            System.out.println("The number is positive.");
+        } else if (num < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        scanner.close();
+    }
+}
