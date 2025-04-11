de61577fa022f7d78bfbe75d3347ffc378b34e5c
vaishnavivs007 <vaishnavishejwadkar@gmail.com>
2025-04-11 10:29:13 +0530

    save this java file

---
+++ demo1.java
@@ -0,0 +1,9 @@
+public class demo1{
+     public static void(String[] args){
+        String name = "abc";
+        if(args!=null&&args.length>0){
+            name=args[0];
+            System.out,println("Hello"+name+"Welcome to java programming")
+        }
+     }
+}
