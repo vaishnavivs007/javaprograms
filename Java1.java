1ee942cca18efcf944d0e8f5afc9b7090e22738f
vaishnavivs007 <vaishnavishejwadkar@gmail.com>
2025-04-11 10:25:23 +0530

    save this java file

---
+++ Java1.java
@@ -0,0 +1,13 @@
+public class Java1{
+    static void myStarticMethod(){
+        System.out.println("Static method can be called without creating object");
+    }
+    public void my public method(){
+         System.out.println("Static method must be called by creating object");
+    }
+    public static void Java1(String[] args){
+        myStaticMethod{},
+        Java1 myobj = new Java1();
+        myobj. mypublicmethod();
+    }
+}
